d4c036df5e3c39dc4cc585fe600386a6d5727727
TechSolanki <mradulpratapsolanki@gmail.com>
2026-06-24 11:06:39 +0530

    Create build.gradle.kts

---
+++ MissAv /build.gradle.kts
@@ -0,0 +1,8 @@
+cloudstream {
+    authors     = listOf("Techmind09")
+    language    = "en" // Plugin ki primary language English rakhein
+    description = "Best Japan AV porn site, supports English and Hindi subtitles, free forever, high speed, no lag, over 100,000 videos with daily update."
+    status  = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://missav.ws/missav/logo-square.png"
+}
